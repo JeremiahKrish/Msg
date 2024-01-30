b8edf415d7e9f06fb09efb8e33934deb6055a775
Krishraj <145432983+JeremiahKrish@users.noreply.github.com>
2024-01-30 10:08:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+script.js 
+
+x = document.getElementById('touch')
+but = document.getElementById('button')
+rem = document.getElementById('remove')
+
+but.addEventListener('click', ()=>{
+    x.style.display = 'none'
+    rem.style.display = 'block'
+    play = new Audio('song.mp3')
+    play.play(); 
+})
